88495e58e549944203d8312c4bd8960740dcf528
Pawan Kumar <ku15101998@gmail.com>
2023-12-09 13:34:24 +0530

    first commit

---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,10 @@
+import SignUp from "../features/Auth/Components/SignUp";
+function SignUpPage() {
+    return ( 
+        <div>
+            <SignUp></SignUp>
+        </div>
+     );
+}
+
+export default SignUpPage;
